1850421bc792748d302996439325c338b97cf89e
Vadym <minerking112256@gmail.com>
2026-04-25 16:47:19 +0200

    chore: update train aggregate

--- src/TrainBooking.Domain/Trains/Train.cs
+++ src/TrainBooking.Domain/Trains/Train.cs
@@ -1,11 +1,17 @@
 using TrainBooking.Domain.Common.Entities;
 using TrainBooking.Domain.Common.Guards;
+using TrainBooking.Domain.Common.Results;
 using TrainBooking.Domain.Trains.DomainEvents;
+using TrainBooking.Domain.Trains.Errors;
+using TrainBooking.Domain.Trains.ValueObjects;
 
 namespace TrainBooking.Domain.Trains;
 
 public class Train : AggregateRoot
 {
+    private readonly List<Wagon> _wagons = [];
+    public IReadOnlyCollection<Wagon> Wagons => _wagons.AsReadOnly();
+
     public string Name { get; private set; } = string.Empty;
 
     private Train() { }
@@ -22,5 +28,17 @@ public class Train : AggregateRoot
         Guard.Against.StringTooLong(name, 200);
 
         return new Train(Guid.CreateVersion7(), name);
+    }
+
+    public Result AddWagon(int wagonNumber, SeatClass wagonClass)
+    {
+        Guard.Against.NegativeOrZero(wagonNumber);
+        Guard.Against.Null(wagonClass);
+        if (_wagons.Any(w => w.Number == wagonNumber))
+            return TrainErrors.DuplicateWagonNumber(wagonNumber);
+
+        var newWagon = Wagon.Create(Id, wagonNumber, wagonClass);
+        _wagons.Add(newWagon);
+        return Result.Success();
     }
 }
